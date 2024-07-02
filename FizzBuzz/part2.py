15b472ab23e3a73e8184b5ca9e7f3a4705c080ba
Anca Radu <anca.radu@softwire.com>
2024-07-02 10:37:10 +0300

    Finished bonus

--- FizzBuzz/part2.py
+++ FizzBuzz/part2.py
@@ -1,51 +1,58 @@
 import numpy as np
 
-def FizzBuzz(my_range, rules):
-    for number in range(1, my_range + 1):
-        string = ""
-    
-        if number % 11 == 0 and rules[11]:
-            string = "Bong"
-        elif number % 15 == 0 and rules[3] and rules[5]:
-            string = "FizzBuzz"
-        elif number % 3 == 0 and rules[3]:
-            string = "Fizz"
-        elif number % 5 == 0 and rules[5]:
-            string = "Buzz"
-    
-        if number % 7 == 0 and rules[7]:
-            string = string + "Bang"
-    
-        if number % 13 == 0 and rules[13]:
-            where_is_B = string.find("B")
-            if where_is_B == -1:
-                string = string + "Fezz"
-            else:
-                string = string[:where_is_B] + "Fezz" + string[where_is_B:]
-    
-        if number % 17 == 0 and rules[17]:
-            index = 0
-            while index <= len(string):
-                substring = string[0:4]
-                string = string[4:] + substring
-                index += 4
-    
-        if string == "":
+
+def FizzBuzz(the_range, rules):
+    for number in range(1, the_range + 1):
+        tokens = []
+        if number % 11 == 0 and rules[11] == True:
+            tokens.append("Bong")
+        else:
+            if number % 15 == 0 and rules[3] == True and rules[5] == True:
+                tokens.append("Fizz")
+                tokens.append("Buzz")
+            elif number % 3 == 0 and rules[3] == True:
+                tokens.append("Fizz")
+            elif number % 5 == 0 and rules[5] == True:
+                tokens.append("Buzz")
+
+            if number % 7 == 0 and rules[7] == True:
+                tokens.append("Bang")
+
+        if number % 13 == 0 and rules[13] == True:
+            where_is_b = -1
+            for tok in tokens:
+                where_is_b = tok.find("B")
+                if where_is_b != -1:
+                    index = tokens.index(tok)
+                    tokens.insert(index, "Fezz")
+                    break
+
+            if where_is_b == -1:
+                tokens.append("Fezz")
+
+        if number % 17 == 0 and rules[17] == True:
+            tokens.reverse()
+
+        if len(tokens) == 0:
             print(number)
         else:
-            print(string)
+            final_string = ""
+            for tok in tokens:
+                final_string += tok
+            print(final_string)
 
 
-rules = np.full(20, False)
+
+my_rules = np.full(20, False)
 
 my_range = int(input("Enter a range: "))
 
 # User enters the rules to be followed --> ends when 0 is received
 input_no = int(input("Enter rule: "))
 
-while (input_no > 0):
-    rules[input_no] = True
+while input_no > 0:
+    my_rules[input_no] = True
     input_no = int(input("Enter rule: "))
 
 
-FizzBuzz(my_range, rules)
+FizzBuzz(my_range, my_rules)
